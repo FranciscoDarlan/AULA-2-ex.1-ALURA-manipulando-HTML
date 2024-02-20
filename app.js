fd7fd2d667e103300ed00943c8abe779fc20465e
FranciscoDarlan <darlann_c_jpa@hotmail.com>
2024-02-20 15:46:06 -0300

    AULA 2 ex.1 manupando html

---
+++ app.js
@@ -0,0 +1,6 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do Número secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um Número Entre 1 e 10';
+
