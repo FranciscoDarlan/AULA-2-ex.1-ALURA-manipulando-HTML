a4ff637857993a1be50c02f4a73dcf61b0380200
FranciscoDarlan <darlann_c_jpa@hotmail.com>
2024-02-21 21:30:24 -0300

    criando funçoes com parametros e return

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do Número secreto';
+// lembrando que o nome dos parametros eu posso escolher qualquer nome.
+// fiquei perdido no nome dos parametros>>>>
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um Número Entre 1 e 10';
+let numeroGerado = gerarNumeroAleatorio(); // aqui fica o returno da função ..
 
-function verificarChute(){
+function trocaTexto(elementosHtml, FrasesEcolhidas) {
+    let variavel = document.querySelector(elementosHtml);
+    variavel.innerHTML = FrasesEcolhidas;
+} // essa função executa algo na tela e não retorna nada.
+
+trocaTexto('h1', 'Jogo Do Número Secreto');
+trocaTexto('p', 'Escolha um número entre 1 e 10');
+
+console.log(trocaTexto())
+
+function verificarChute() { // essa função sem (params)
     console.log('Fui Clicado!');
 }
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+} // essa função tem retorno queremos que ela nos diga algo ..
+
